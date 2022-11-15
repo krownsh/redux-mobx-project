1b4f8c884dd76dbe627561a166ef551740fcd944
alex <bgkong1205@gmail.com>
2022-11-15 13:35:18 +0800

    'eslint'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import React from 'react'
+const App = () => <div>App</div>;
 
-const App = () => {
-  return (
-    <div>App 3323</div>
-  )
-}
-
-export default App
+export default App;
